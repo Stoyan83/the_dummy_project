50621da226cf9719be4b36770200e92501c4f4d3
Stoyan83 <stoyan_stoyanov@rocketmail.com>
2022-10-27 00:41:42 +0300

    Adding Modal to log in

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,16 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener("turbo:load", function() {
+$(document).ready(function () {
+  $(".nav-toggler").each(function (_, navToggler) {
+    var target = $(navToggler).data("target");
+    $(navToggler).on("click", function () {
+      $(target).animate({
+        height: "toggle",
+      });
+    });
+  });
+});
+});
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do 
+
+  root 'home#index' 
   devise_for :users
-  root 'home#index'  
+
+  devise_scope :user do
+    get '/users' =>  'devise/registrations#new'
+  end
 end
