27d1cddc5c72f03f6f7b232a991787da5eb79380
Stoyan83 <stoyan_stoyanov@rocketmail.com>
2022-10-28 11:05:27 +0300

    Adding sign up form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,12 @@
 class ApplicationController < ActionController::Base
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  protected
 
-  private
+  def configure_permitted_parameters    
+    devise_parameter_sanitizer.permit :sign_up, keys: [:username, :email]    
+    devise_parameter_sanitizer.permit :account_update, keys: [:username, :email]
+  end
 
-  # def after_sign_out_path_for(resource_or_scope)
-  #   root_path
-  # end   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  attr_writer :login      
+  validate :validate_username, on: :create
+  validates_uniqueness_of :username, :email
+
+  validates :password, length: { minimum: 6, maximum: 20 }, on: :create
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }, length: {maximum: 320} 
+  validates :username, presence: true, length: { in: 3..40 }, format: { with: /\A[a-zA-Z0-9]+\z/, on: :create,message: "empty spaces not allowed" } 
+
+  
+  def validate_username
+    if User.where(username: username).exists?
+      errors.add(:username, :invalid)
+    end
+  end  
 end
+
