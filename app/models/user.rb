31c04f2b261b001eaaa29a34301b39c175eb399a
Stoyan83 <stoyan_stoyanov@rocketmail.com>
2022-11-02 16:05:06 +0200

    Refactorin header and layouts

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  attr_writer :login    
   has_many :pages, dependent: :destroy
   has_one_attached :avatar 
 
